4d30b5337ce9f1a66d52a8735332c666472656fb
Viktor He <viktor.he@yahoo.com>
2019-08-05 21:48:25 +0700

    Before

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ItemList from './components/item'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('render create element div', () =>{
+  const container = document.createElement('div')
+  ReactDOM.render(<ItemList items={[]} />,container )
+  expect(container).toBeDefined()
+})
+
+test('renders "no items" when the item list is empty', () => {
+  const container = document.createElement('div')
+  ReactDOM.render(<ItemList items={[]} />,container )
+  expect(container.textContent).toMatch('no items')
+})
